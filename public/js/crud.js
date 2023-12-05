b4a819176f01bcaee3e669243bb0ef9b5ab220b2
Ruben Ortiz <oruben2@gmail.com>
2023-12-05 10:55:42 -0600

    jala5r

--- public/js/crud.js
+++ public/js/crud.js
@@ -2,12 +2,12 @@ const contenedorProductos = document.getElementById('productos');
 
 // Función para mostrar los productos en el HTML
 function mostrarProductos(productos) {
-  contenedorProductos.innerHTML = ''; // Limpiar el contenedor
+    contenedorProductos.innerHTML = ''; // Limpiar el contenedor
 
-  productos.forEach(producto => {
-    // Crear elementos HTML para cada producto
-    const productoDiv = document.createElement('div');
-    productoDiv.innerHTML = `
+    productos.forEach(producto => {
+        // Crear elementos HTML para cada producto
+        const productoDiv = document.createElement('div');
+        productoDiv.innerHTML = `
       <h2>${producto.nombre}</h2>
       <p>Precio: $${producto.precio}</p>
       <p>${producto.descripcion_corta}</p>
@@ -16,103 +16,112 @@ function mostrarProductos(productos) {
       <button onclick="eliminarProducto('${producto.nombre}')">Eliminar</button>
     `;
 
-    // Agregar el producto al contenedor
-    contenedorProductos.appendChild(productoDiv);
-  });
+        // Agregar el producto al contenedor
+        contenedorProductos.appendChild(productoDiv);
+    });
 }
 
 // Función para editar un producto (puedes implementarla según tu necesidad)
 function editarProducto(nombre) {
-  // Lógica para editar un producto
-  console.log(`Editar ${nombre}`);
+    // Lógica para editar un producto
+    console.log(`Editar ${nombre}`);
 }
 
 // Función para eliminar un producto (puedes implementarla según tu necesidad)
 function eliminarProducto(nombre) {
-  // Lógica para eliminar un producto
-  console.log(`Eliminar ${nombre}`);
+    // Lógica para eliminar un producto
+    console.log(`Eliminar ${nombre}`);
 }
 
 // Cargar los productos desde el archivo JSON
 fetch('productos.json')
-  .then(response => response.json())
-  .then(data => {
-  
-    mostrarProductos(data);
-  })
-  .catch(error => {
-    console.error('Error al cargar los productos:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+
+        mostrarProductos(data);
+    })
+    .catch(error => {
+        console.error('Error al cargar los productos:', error);
+    });
 
 
-  console.log("correct");
+console.log("correct");
 
-  document.querySelector('#btnExtraer').addEventListener('click', traerDatos());
-  
-  
-  function traerDatos() {
-      console.log("correct");
-  
-      const xhttp = new XMLHttpRequest();
-  
-      xhttp.open('GET', 'productos.json', true);
-  
-      xhttp.send();
-  
-      xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-              console.log(this.responseText);
-              let datos = JSON.parse(this.responseText);
-              console.log(datos)
-           
-              let res = document.querySelector('#res');
-              res.innerHTML = '';
-  
-  
-              for (let item of datos) {
-                  //console.log(item.precio);
-                  
-                  res.innerHTML += "<tr><td>"+item.nombre+"</td><td>"+item.precio+"</td><td>"+item.descripcion_corta+"</td><td><td>"+item.descripcion_larga+"</td><td> <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br></a></td></tr>"
-              }
-   
-          }
-      }
-  }  
+document.querySelector('#btnExtraer').addEventListener('click', traerDatos());
 
-  console.log("correct");
 
-  document.querySelector('#eliminar').addEventListener('click', traer);
-  
-  
-  function traer() {
-      console.log("correct");
-  
-      const xhttp = new XMLHttpRequest();
-  
-      xhttp.open('GET', 'productos.json', true);
-  
-      xhttp.send();
-  
-      xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-              console.log(this.responseText);
-              let datos = JSON.parse(this.responseText);
-              console.log(datos)
-           
-              let res = document.querySelector('#res');
-              res.innerHTML = '';
-  
-  
-              for (let item of datos) {
-                  //console.log(item.precio);
-                  
-                  res.innerHTML += "<tr><td>"+item.nombre+"</td><td>"+item.precio+"</td><td>"+item.descripcion_corta+"</td><td>"+item.descripcion_larga+"</td><td> <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br></a> <a href='#' class='btn btn-primary' onclick=''>Editar</a></td></tr>"
-              
-                if(item.id==9){
-                  datos.splice(1, 1)
+function traerDatos() {
+    console.log("correct");
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', 'productos.json', true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            let datos = JSON.parse(this.responseText);
+            console.log(datos)
+
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+
+            for (let item of datos) {
+                //console.log(item.precio);
+
+                res.innerHTML += "<tr><td>" + item.nombre + "</td><td>" + item.precio + "</td><td>" + item.descripcion_corta + "</td><td><td>" + item.descripcion_larga + "</td><td> <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br></a></td></tr>"
+            }
+
+        }
+    }
+}
+
+console.log("correct");
+
+document.querySelector('#eliminar').addEventListener('click', traer);
+
+
+function traer() {
+    console.log("correct");
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', 'productos.json', true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            let datos = JSON.parse(this.responseText);
+            console.log(datos)
+
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+
+            for (let item of datos) {
+                //console.log(item.precio);
+
+                res.innerHTML += `
+    <tr>
+        <td>${item.nombre}</td>
+        <td>${item.precio}</td>
+        <td>${item.descripcion_corta}</td>
+        <td>${item.descripcion_larga}</td>
+        <td>
+            <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br>
+            <a href='#' class='btn btn-primary' onclick=''>Editar</a>
+        </td>
+    </tr>`;
+
+                if (item.id == 9) {
+                    datos.splice(1, 1)
                 }
-              }      
-          }
-      }
-  }
-  
+            }
+        }
+    }
+}
