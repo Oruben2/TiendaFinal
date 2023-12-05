699d17149f900533229078591569532f8d0feae1
Ruben Ortiz <oruben2@gmail.com>
2023-12-05 08:56:23 -0600

    Prueba crud de productos 2

--- public/js/crud.js
+++ public/js/crud.js
@@ -72,7 +72,7 @@ fetch('productos.json')
               for (let item of datos) {
                   //console.log(item.precio);
                   
-                  res.innerHTML += "<tr><td>"+item.id+"</td><td>"+item.nombre+"</td><td>"+item.cantidad+"</td><td>"+item.descripción+"</td><td>"+item.precio+"</td><td><img src='"+item.imagen+"'></img></td><td>  <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br> <a href='Modificar.html' class='btn btn-primary' onclick=''>Modificar</a></td></tr>"
+                  res.innerHTML += "<tr><td>"+item.id+"</td><td>"+item.nombre+"</td><td>"+item.precio+"</td><td>"+item.descripcion_larga+"</td><td> <a href='#' class='btn btn-primary' onclick=''>Eliminar</a><br><br></a></td></tr>"
    
               }
    
