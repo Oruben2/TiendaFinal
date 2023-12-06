63048071c17161af6f5c02448c03f6b7dc782f55
Ruben Ortiz <oruben2@gmail.com>
2023-12-06 04:17:10 -0600

    -

--- public/js/producto.js
+++ public/js/producto.js
@@ -1,20 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btnEnviarWishlist").addEventListener("click", function() {
-      fetch('productos.json')
-        .then(response => response.json())
-        .then(data => {
-          // Obtener el primer producto del JSON
-          const primerProducto = data[0];
-  
-          // Redirigir a wishlist.html con los datos del producto como parámetros en la URL
-          const urlParams = new URLSearchParams();
-          urlParams.append('nombre', primerProducto.nombre);
-          urlParams.append('descripcion', primerProducto.descripcion_corta);
-          urlParams.append('precio', primerProducto.precio);
-  
-          window.location.href = `wishlist.html?${urlParams.toString()}`;
-        })
-        .catch(error => console.log('Error:', error));
-    });
-  });
-  
+// Función para mostrar el producto en la wishlist al cargar la página
+function mostrarProductoEnWishlist() {
+  // El primer producto de tu JSON
+  const primerProducto = {
+    "nombre": "Enfriador de CPU con RGB",
+    "precio": 1569,
+    "descripcion_corta": "Marca: COOLER MASTER Producto de alta calidad RGB"
+  };
+
+  // Creamos un elemento para mostrar la información del producto en la wishlist
+  const productoHTML = `
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${primerProducto.nombre}</h5>
+          <p class="card-text">${primerProducto.descripcion_corta}</p>
+          <p class="card-text">Precio: $${primerProducto.precio}</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Agregamos el producto al contenedor de la wishlist
+  document.getElementById('wishlist').innerHTML = productoHTML;
+}
+
+// Llamamos a la función para mostrar el producto en la wishlist al cargar la página
+window.onload = mostrarProductoEnWishlist;
--- public/js/wishlist.js
+++ public/js/wishlist.js
@@ -1,14 +1,4 @@
-// Función para agregar productos a la Wishlist
-function agregarProducto(nombreProducto) {
-    // Obtener la lista de deseos
-    const wishlist = document.getElementById('wishlist');
-  
-    // Crear un nuevo elemento de lista
-    const nuevoProducto = document.createElement('li');
-    nuevoProducto.className = 'list-group-item';
-    nuevoProducto.innerText = nombreProducto;
-  
-    // Agregar el producto a la lista de deseos
-    wishlist.appendChild(nuevoProducto);
-  }
-  
+   // Función para redirigir a la página de wishlist al hacer clic en "Agregar a Wishlist"
+   document.getElementById('agregarWishlist').addEventListener('click', function() {
+    window.location.href = 'wishlist.html';
+  });
