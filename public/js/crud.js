abfcc16abf3a69a61929764594a084981768ba82
Ruben Ortiz <oruben2@gmail.com>
2023-12-06 03:11:08 -0600

    funciones eliminar y editar

--- public/js/crud.js
+++ public/js/crud.js
@@ -12,7 +12,7 @@ function mostrarProductos(data) {
       <td>${item.descripcion_larga}</td>
       <td>
         <a href='#' class='btn btn-danger' onclick='eliminarProducto()'>Eliminar</a><br><br>
-        <a href='#' class='btn btn-succes' onclick='editarProducto()'>Editar</a>
+        <a href='#' class='btn btn-success' onclick='editarProducto()'>Editar</a>
       </td>
     `;
     cuerpoTabla.appendChild(fila);
@@ -34,6 +34,10 @@ function mostrarUsuarios() {
           <td>${usuario.correo}</td>
           <td>${usuario.contraseña}</td>
           <td>${usuario.ciudad}</td>
+          <td>
+          <a href='#' class='btn btn-danger' onclick='eliminarProducto()'>Eliminar</a><br><br>
+          <a href='#' class='btn btn-success' onclick='editarProducto()'>Editar</a>
+        </td>
         `;
         cuerpoTabla.appendChild(fila);
       });
@@ -41,6 +45,24 @@ function mostrarUsuarios() {
     .catch(error => {
       console.error('Error al cargar los usuarios:', error);
     });
+}
+
+function eliminarProducto(index) {
+  // Aquí puedes eliminar el producto del array 'data' utilizando 'index'
+  // Por ejemplo, si 'data' es un array, puedes usar 'splice' para eliminarlo
+  data.splice(index, 1);
+// Función para editar producto
+function editarProducto(index) {
+  // Aquí puedes realizar acciones para editar el producto en el array 'data' utilizando 'index'
+  // Por ejemplo, abrir un formulario de edición con los datos del producto seleccionado
+
+  // Luego, podrías guardar los cambios realizados y actualizar la tabla
+  // renderizarTabla();
+}
+
+// Función para renderizar la tabla nuevamente con los datos actualizados
+function renderizarTabla() {
+  // Código para limpiar la tabla actual y volver a renderizarla con los datos actualizados
 }
 
 // Llamar a las funciones para mostrar los usuarios y los productos al cargar la página
