1043e9d9f9efd400250f5deaf64515edd01e07e0
Ruben Ortiz <oruben2@gmail.com>
2023-12-12 03:13:23 -0600

    aaa

--- public/js/producto.js
+++ public/js/producto.js
@@ -1,42 +1,55 @@
-// Función para mostrar el producto en la wishlist al cargar la página
+class Producto {
+  constructor(nombre, descripcion, precio) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+  }
+
+  generarHTML() {
+    return `
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${this.nombre}</h5>
+            <p class="card-text">${this.descripcion}</p>
+            <p class="card-text">Precio: $${this.precio}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
 function mostrarProductoEnWishlist() {
   // El primer producto de tu JSON
   fetch('productos.json')
-  .then(response => response.json())
-  .then(productos => {
-    // Aquí puedes trabajar con la lista de productos obtenidos
-    console.log(productos); // Por ejemplo, muestra los productos en la consola
-    // También puedes manipularlos, mostrarlos en la página, etc.
-  })
-  .catch(error => console.error('Error al obtener los datos:', error));
+    .then(response => response.json())
+    .then(productos => {
+      const primerProducto = new Producto(
+        productos[0].nombre,
+        productos[0].descripcion_corta,
+        productos[0].precio
+      );
+      const segundoProducto = new Producto(
+        productos[1].nombre,
+        productos[1].descripcion_corta,
+        productos[1].precio
+      );
 
+      const productoHTML = primerProducto.generarHTML();
+      
+      // Agregamos el producto al contenedor de la wishlist
+      document.getElementById('wishlist').innerHTML = productoHTML;
 
-  // Creamos un elemento para mostrar la información del producto en la wishlist
-  const primerProducto = productos[0];
-
-  const productoHTML = `
-  <div class="col-md-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">${primerProducto.nombre}</h5>
-        <p class="card-text">${primerProducto.descripcion_corta}</p>
-        <p class="card-text">Precio: $${primerProducto.precio}</p>
-      </div>
-    </div>
-  </div>
-  `;
-  
-  console.log([productoHTML]);
-  
-  // Agregamos el producto al contenedor de la wishlist
-  document.getElementById('wishlist').innerHTML = productoHTML;
-    // Agregamos el producto al contenedor de la wishlist
-    document.getElementById('carrito').innerHTML = productoHTML;
+      // Manejador de clic para agregar el producto al carrito
+      document.getElementById('wishlist').addEventListener('click', () => {
+        // Aquí puedes ejecutar la lógica para agregar el producto al carrito
+        const productoEnCarritoHTML = primerProducto.generarHTML();
+        document.getElementById('carrito').innerHTML = productoEnCarritoHTML;
+      });
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
 }
 
 // Llamamos a la función para mostrar el producto en la wishlist al cargar la página
 window.onload = mostrarProductoEnWishlist;
-
-// Llamamos a la función para mostrar el producto en la wishlist al cargar la página
-window.onload = mostrarProductoEnWishlist;
-
